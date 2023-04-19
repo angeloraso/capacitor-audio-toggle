e09ddd240484a48340deb5fadbf3dec6f7ab30e2
Angelo Raso <araso@anura.com.ar>
2023-04-19 15:50:40 -0300

    Initial commit

---
+++ android/src/main/java/ar/com/anura/plugins/audiotoggle/AudioToggle.java
@@ -0,0 +1,11 @@
+package ar.com.anura.plugins.audiotoggle;
+
+import android.util.Log;
+
+public class AudioToggle {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
